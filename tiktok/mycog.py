f1062900b99701a4d8248c50ee3312715ff85d53
3 <74258466+aerolution@users.noreply.github.com>
2023-05-11 22:56:24 -0400

    Update mycog.py

--- tiktok/mycog.py
+++ tiktok/mycog.py
@@ -1,2 +1,34 @@
+import os
+import asyncio
+import discord
+from tiktokpy import TikTokPy
+from redbot.core import commands
+api = TikTokPy()
+
+class MyCog(commands.Cog):
+    """My custom cog"""
+
+    def __init__(self, bot):
+        self.bot = bot
 
 
+    @commands.command()
+async def fyp(ctx):
+    print('Executing trendingvids command...')
+    try:
+        # Fetch the trending videos from TikTok API
+        trending = api.trending(count=5)
+
+        # Filter the videos with #dogs and #cats hashtags
+        filtered = [video for video in trending if "#dogs" in video['desc'] and "#cats" in video['desc']]
+
+        # Create the response message
+        response = ""
+        for i, video in enumerate(filtered):
+            response += f"{i+1}. {video['desc']} - {video['video']['urls'][0]}\n"
+
+        print('Sending response...')
+        # Send the response to the Discord channel
+        await ctx.send(response)
+    except Exception as e:
+        print('Error:', e)
