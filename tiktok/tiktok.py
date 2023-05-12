be6a7872d8bf45cad69ddc7bc13174e2d983f015
3 <74258466+aerolution@users.noreply.github.com>
2023-05-12 11:37:26 -0400

    Update tiktok.py

--- tiktok/tiktok.py
+++ tiktok/tiktok.py
@@ -2,7 +2,7 @@ import discord
 from redbot.core import commands
 from tikapi import TikAPI, ValidationException, ResponseException
 
-class fyp(commands.Cog):
+class TikTokUser(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.api_key = "hnxoYBFNO7uQFUpmRkiPNmb3Rr11YJYerx7clmfc7mHRpPWS"
@@ -16,12 +16,12 @@ class fyp(commands.Cog):
             user = api.user(accountKey=self.user_account_key)
             response = user.info()
             response_json = response.json()
-            user_info = f"Username: {response_json['user']['uniqueId']}\n" \
-                        f"Full Name: {response_json['user']['nickname']}\n" \
-                        f"Bio: {response_json['user']['signature']}\n" \
-                        f"Following: {response_json['stats']['followingCount']}\n" \
-                        f"Followers: {response_json['stats']['followerCount']}\n" \
-                        f"Total Likes: {response_json['stats']['heartCount']}"
+            user_info = f"Username: {response_json['itemInfos'][0]['itemStruct']['author']['uniqueId']}\n" \
+                        f"Full Name: {response_json['itemInfos'][0]['itemStruct']['author']['nickname']}\n" \
+                        f"Bio: {response_json['itemInfos'][0]['itemStruct']['author']['signature']}\n" \
+                        f"Following: {response_json['itemInfos'][0]['itemStruct']['authorStats']['followingCount']}\n" \
+                        f"Followers: {response_json['itemInfos'][0]['itemStruct']['authorStats']['followerCount']}\n" \
+                        f"Total Likes: {response_json['itemInfos'][0]['itemStruct']['authorStats']['heartCount']}"
 
             await ctx.send(user_info)
         except ValidationException as e:
@@ -30,4 +30,5 @@ class fyp(commands.Cog):
             await ctx.send(f"Response error: {e}, {e.response.status_code}")
         except Exception as e:
             await ctx.send(f"Error fetching user info: {e}")
+
 
