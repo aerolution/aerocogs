cce79f829bae1ba0ac05b71b4901948357f60f13
3 <74258466+aerolution@users.noreply.github.com>
2023-05-11 23:11:12 -0400

    Update tiktok.py

--- tiktok/tiktok.py
+++ tiktok/tiktok.py
@@ -4,13 +4,11 @@ import discord
 from tiktokpy import TikTokPy
 from redbot.core import commands
 
-api = TikTokPy(headers={
-  'accept': 'application/json, text/plain, */*',
-  'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-  'referer': 'https://www.tiktok.com/',
-  'sec-fetch-dest': 'empty',
-  'sec-fetch-mode': 'cors',
-  'sec-fetch-site': 'same-origin'
+api = TikTokPy({
+  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+  'Accept-Language': 'en-US,en;q=0.5',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Upgrade-Insecure-Requests': '1'
 })
 
 class fyp(commands.Cog):
