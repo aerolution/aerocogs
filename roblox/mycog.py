8b14cc6afd6dd2d1637178aaa8fdaad7da7b32a2
3 <74258466+aerolution@users.noreply.github.com>
2023-05-11 22:10:53 -0400

    Update mycog.py

--- roblox/mycog.py
+++ roblox/mycog.py
@@ -8,8 +8,9 @@ class MyCog(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def roblox(ctx, username):
-        users_json = requests.get(f"https://www.roblox.com/search/users/results?keyword={username}&maxRows=1&startIndex=0")
+       async def rouser(ctx, username):
+ 
+    users_json = requests.get(f"https://www.roblox.com/search/users/results?keyword={username}&maxRows=1&startIndex=0")
     users = json.loads(users_json.text)
     user_id = users['UserSearchResults'][0]['UserId']
  
@@ -26,11 +27,13 @@ class MyCog(commands.Cog):
     thumbnail_url = thumbnail['data'][0]['imageUrl']
  
     embed = discord.Embed(title=f"{username}", url=f"https://www.roblox.com/users/{user_id}/profile", color=0x00b3ff)
-    embed.set_author(name="ROBLOX")
+    embed.set_author(name="RoUser")
+    embed.set_footer(text="Made by AllysonStudiosDev")
  
     embed.add_field(name="id", value=f"{user_id}", inline=False)
     embed.add_field(name="Displayname", value=f"{display_name}", inline=True)
     embed.add_field(name="created", value=f"{created_date}", inline=False)
     embed.add_field(name="description", value=f"{description}", inline=True)
     embed.set_thumbnail(url=f"{thumbnail_url}")
-    await ctx.send(embed=embed))
+    await ctx.send(embed=embed)
+
