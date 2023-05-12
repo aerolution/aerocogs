8c09ed2d379c2ec25919be20a5c63dbb2d0deb1e
3 <74258466+aerolution@users.noreply.github.com>
2023-05-12 11:06:18 -0400

    Update tiktok.py

--- tiktok/tiktok.py
+++ tiktok/tiktok.py
@@ -1,19 +1,19 @@
 import discord
 from redbot.core import commands
-from tikapi import TikAPI
+from tikapi import TikApi
 
-class fyp(commands.Cog):
+class TikTok(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.tiktok_api = TikAPI("hnxoYBFNO7uQFUpmRkiPNmb3Rr11YJYerx7clmfc7mHRpPWS")
+        self.tiktok_api = TikApi("YOUR_API_KEY")
 
     @commands.command()
-    async def fyp(self, ctx, username):
+    async def tiktok(self, ctx, username):
         """Fetches a user's latest TikTok video."""
         try:
-            user_videos = self.tiktok_api.getUser(username)
+            user_videos = self.tiktok_api.get_user_videos(username)
             latest_video = user_videos[0]
-            video_url = latest_video['video']['downloadAddr']
+            video_url = latest_video.video_url
             await ctx.send(f"Latest TikTok video from {username}: {video_url}")
         except Exception as e:
             await ctx.send(f"Error fetching TikTok video: {e}")
