6dfe51664d2d8acbbec6b1ada8a131f59ae82311
3 <74258466+aerolution@users.noreply.github.com>
2023-05-12 01:37:16 -0400

    Update lock.py

--- lock/lock.py
+++ lock/lock.py
@@ -1,29 +1,53 @@
 import discord
 from discord.ext import commands
+import requests
+import json
 
-class Lockdown(commands.Cog):
+class Lock(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.locked_channels = set()
 
     @commands.command()
     @commands.has_permissions(manage_channels=True)
-    async def lockdown(self, ctx):
-        channel = ctx.channel
-        everyone = channel.guild.default_role
-        bot_member = channel.guild.me
-        overwrite = channel.overwrites_for(everyone)
+    async def lock(self, ctx):
+        if ctx.channel.id in self.locked_channels:
+            await ctx.send("Channel is already locked.")
+            return
 
-        if overwrite.send_messages is None or overwrite.send_messages:
-            # Channel is not locked, so we need to lock it
-            overwrite.send_messages = False
-            await channel.set_permissions(everyone, overwrite=overwrite)
-            await ctx.send(f"{bot_member.mention} has **locked** the channel {channel.mention}.")
+        # Update permissions to deny sending messages for everyone
+        await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=False)
 
-        else:
-            # Channel is already locked, so we need to unlock it
-            overwrite.send_messages = True
-            await channel.set_permissions(everyone, overwrite=overwrite)
-            await ctx.send(f"{bot_member.mention} has **unlocked** the channel {channel.mention}.")
+        # Get lock GIF using GIPHY API
+        response = requests.get(f"https://api.giphy.com/v1/gifs/random?api_key=<your_api_key>&tag=lock")
+        gif_data = json.loads(response.text)
+        gif_url = gif_data["data"]["image_original_url"]
+
+        # Create embed message with lock GIF
+        embed = discord.Embed()
+        embed.title = "Channel Locked"
+        embed.description = "This channel has been temporarily locked."
+        embed.set_image(url=gif_url)
+        await ctx.send(embed=embed)
+
+        self.locked_channels.add(ctx.channel.id)
+
+    @commands.command()
+    @commands.has_permissions(manage_channels=True)
+    async def unlock(self, ctx):
+        if ctx.channel.id not in self.locked_channels:
+            await ctx.send("Channel is not locked.")
+            return
+
+        # Update permissions to allow sending messages for everyone
+        await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
+
+        embed = discord.Embed()
+        embed.title = "Channel Unlocked"
+        embed.description = "This channel has been unlocked."
+        await ctx.send(embed=embed)
+
+        self.locked_channels.remove(ctx.channel.id)
 
 def setup(bot):
-    bot.add_cog(Lockdown(bot))
+    bot.add_cog(Lock(bot))
