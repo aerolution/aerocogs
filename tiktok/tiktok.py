aa0732b6eb23b4db7bfe87746cfcb41f63c4d680
3 <74258466+aerolution@users.noreply.github.com>
2023-05-12 11:40:38 -0400

    Update tiktok.py

--- tiktok/tiktok.py
+++ tiktok/tiktok.py
@@ -16,12 +16,12 @@ class fyp(commands.Cog):
             user = api.user(accountKey=self.user_account_key)
             response = user.info()
             response_json = response.json()
-            user_info = f"Username: {response_json['itemInfos'][0]['itemStruct']['author']['uniqueId']}\n" \
-                        f"Full Name: {response_json['itemInfos'][0]['itemStruct']['author']['nickname']}\n" \
-                        f"Bio: {response_json['itemInfos'][0]['itemStruct']['author']['signature']}\n" \
-                        f"Following: {response_json['itemInfos'][0]['itemStruct']['authorStats']['followingCount']}\n" \
-                        f"Followers: {response_json['itemInfos'][0]['itemStruct']['authorStats']['followerCount']}\n" \
-                        f"Total Likes: {response_json['itemInfos'][0]['itemStruct']['authorStats']['heartCount']}"
+            user_info = f"Username: {response_json['itemInfo']['itemStruct']['author']['uniqueId']}\n" \
+                        f"Full Name: {response_json['itemInfo']['itemStruct']['author']['nickname']}\n" \
+                        f"Bio: {response_json['itemInfo']['itemStruct']['author']['signature']}\n" \
+                        f"Following: {response_json['itemInfo']['itemStruct']['stats']['followingCount']}\n" \
+                        f"Followers: {response_json['itemInfo']['itemStruct']['stats']['followerCount']}\n" \
+                        f"Total Likes: {response_json['itemInfo']['itemStruct']['stats']['heartCount']}"
 
             await ctx.send(user_info)
         except ValidationException as e:
