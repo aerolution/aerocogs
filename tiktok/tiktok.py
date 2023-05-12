ceae397da44af91633f645068cad67e9faeeea50
3 <74258466+aerolution@users.noreply.github.com>
2023-05-12 11:07:34 -0400

    Update tiktok.py

--- tiktok/tiktok.py
+++ tiktok/tiktok.py
@@ -2,13 +2,13 @@ import discord
 from redbot.core import commands
 from tikapi import TikAPI
 
-class TikTok(commands.Cog):
+class fyp(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.tiktok_api = TikApi("YOUR_API_KEY")
 
     @commands.command()
-    async def tiktok(self, ctx, username):
+    async def fyp(self, ctx, username):
         """Fetches a user's latest TikTok video."""
         try:
             user_videos = self.tiktok_api.get_user_videos(username)
