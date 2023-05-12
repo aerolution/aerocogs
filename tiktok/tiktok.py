489fee140dc29d793c46c8b97a98fa0c41fde7ef
3 <74258466+aerolution@users.noreply.github.com>
2023-05-12 12:59:34 -0400

    Update tiktok.py

--- tiktok/tiktok.py
+++ tiktok/tiktok.py
@@ -14,14 +14,17 @@ class fyp(commands.Cog):
         try:
             response = self.user.posts.video(id=video_id)
             video_info = response.json()
-            embed = discord.Embed(title="TikTok Video Information")
-            embed.add_field(name="Video ID", value=video_info["id"], inline=False)
-            embed.add_field(name="Description", value=video_info["desc"], inline=False)
-            embed.add_field(name="Likes", value=video_info["stats"]["diggCount"], inline=True)
-            embed.add_field(name="Shares", value=video_info["stats"]["shareCount"], inline=True)
-            embed.add_field(name="Comments", value=video_info["stats"]["commentCount"], inline=True)
-            embed.add_field(name="Plays", value=video_info["stats"]["playCount"], inline=True)
-            await ctx.send(embed=embed)
+            if "id" in video_info:
+                embed = discord.Embed(title="TikTok Video Information")
+                embed.add_field(name="Video ID", value=video_info["id"], inline=False)
+                embed.add_field(name="Description", value=video_info["desc"], inline=False)
+                embed.add_field(name="Likes", value=video_info["stats"]["diggCount"], inline=True)
+                embed.add_field(name="Shares", value=video_info["stats"]["shareCount"], inline=True)
+                embed.add_field(name="Comments", value=video_info["stats"]["commentCount"], inline=True)
+                embed.add_field(name="Plays", value=video_info["stats"]["playCount"], inline=True)
+                await ctx.send(embed=embed)
+            else:
+                await ctx.send("Video information not found.")
         except ValidationException as e:
             await ctx.send(f"Validation error: {e}, field: {e.field}")
         except ResponseException as e:
