c7c8b5b4f937fa5e5888eea1961bd7ec4b3fee5f
3 <74258466+aerolution@users.noreply.github.com>
2023-05-11 23:59:59 -0400

    Update tiktok.py

--- tiktok/tiktok.py
+++ tiktok/tiktok.py
@@ -1,10 +1,8 @@
-import os
 import asyncio
 import discord
 from redbot.core import commands
-from tiktokapi import TikTokApi
-
-api = TikTokApi.get_instance()
+import requests
+from bs4 import BeautifulSoup
 
 class MyCog(commands.Cog):
     """Get trending TikTok videos"""
@@ -15,12 +13,14 @@ class MyCog(commands.Cog):
     @commands.command()
     async def tiktok(self, ctx):
         """Get the trending TikTok videos"""
-        trending = api.trending(count=5)
-        # Filter the videos with dogs and cats hashtags
-        filtered = [video for video in trending if "#dogs" in video['desc'] and "#cats" in video['desc']]
+        url = "https://www.tiktok.com/en/trending"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, "html.parser")
+        videos = soup.find_all("h2", {"class": "jsx-3852143988"})
+        random_video = videos[random.randint(0, len(videos)-1)]
+        video_title = random_video.text
+        video_url = "https://www.tiktok.com" + random_video.parent.get("href")
         # Create the response message
-        response = ""
-        for i, video in enumerate(filtered):
-            response += f"{i+1}. {video['desc']} - {video['video']['downloadAddr']}\n"
+        response = f"Here's a trending TikTok video: {video_title} - {video_url}"
         # Send the response to the Discord channel
         await ctx.send(response)
