a3f720c8ab5c4e562b1b9cd735717394679e52b5
3 <74258466+aerolution@users.noreply.github.com>
2023-05-11 23:13:23 -0400

    Update tiktok.py

--- tiktok/tiktok.py
+++ tiktok/tiktok.py
@@ -1,8 +1,8 @@
 import os
 import asyncio
 import discord
-from tiktokpy import TikTokPy
 from redbot.core import commands
+from tiktokpy import TikTokPy
 
 api = TikTokPy({
   'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
@@ -11,29 +11,31 @@ api = TikTokPy({
   'Upgrade-Insecure-Requests': '1'
 })
 
-class fyp(commands.Cog):
-    """My custom cog"""
+class MyCog(commands.Cog):
+    """A custom cog to get trending TikTok videos"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
-    async def fyp(self, ctx):
-      print('Executing trendingvids command...')
-      try:
-          # Fetch the trending videos from TikTok API
-          trending = api.trending(count=5)
+    async def tiktok(self, ctx):
+        """Get the trending TikTok videos"""
+        print('Executing tiktok command...')
+        try:
+            # Fetch the trending videos from TikTok API
+            trending = api.trending(count=5)
 
-          # Filter the videos with #dogs and #cats hashtags
-          filtered = [video for video in trending if "#dogs" in video['desc'] and "#cats" in video['desc']]
+            # Filter the videos with #dogs and #cats hashtags
+            filtered = [video for video in trending if "#dogs" in video['desc'] and "#cats" in video['desc']]
 
-          # Create the response message
-          response = ""
-          for i, video in enumerate(filtered):
-              response += f"{i+1}. {video['desc']} - {video['video']['urls'][0]}\n"
+            # Create the response message
+            response = ""
+            for i, video in enumerate(filtered):
+                response += f"{i+1}. {video['desc']} - {video['video']['urls'][0]}\n"
 
-          print('Sending response...')
-          # Send the response to the Discord channel
-          await ctx.send(response)
-      except Exception as e:
-          print('Error:', e)
+            print('Sending response...')
+            # Send the response to the Discord channel
+            await ctx.send(response)
+
+        except Exception as e:
+            print('Error:', e)
