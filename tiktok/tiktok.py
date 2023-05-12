d98a7922f362591b4c5e610930f4d9f473068dbb
3 <74258466+aerolution@users.noreply.github.com>
2023-05-12 00:09:33 -0400

    Update tiktok.py

--- tiktok/tiktok.py
+++ tiktok/tiktok.py
@@ -18,10 +18,14 @@ class fyp(commands.Cog):
         response = requests.get(url)
         soup = BeautifulSoup(response.text, "html.parser")
         videos = soup.find_all("h2", {"class": "jsx-3852143988"})
-        random_video = videos[random.randint(0, len(videos)-1)]
+        if not videos:
+            await ctx.send("Sorry, there are no trending TikTok videos available right now.")
+            return
+        random_video = random.choice(videos)
         video_title = random_video.text
         video_url = "https://www.tiktok.com" + random_video.parent.get("href")
         # Create the response message
         response = f"Here's a trending TikTok video: {video_title} - {video_url}"
         # Send the response to the Discord channel
         await ctx.send(response)
+
