592f535c96653ffaefd0a65c3ef4541d23358db3
3 <74258466+aerolution@users.noreply.github.com>
2023-05-12 10:22:36 -0400

    Update tiktok.py

--- tiktok/tiktok.py
+++ tiktok/tiktok.py
@@ -4,21 +4,21 @@ import httpx
 import discord
 from redbot.core import commands
 from TikTokApi import TikTokApi
+from tiktok_scraper import hashtags, user
 
 class fyp(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-        self.api = TikTokApi()
 
     @commands.command()
     async def fyp(self, ctx):
         # Fetch trending TikTok videos
-        trending_videos = self.api.trending(count=1)
+        videos = hashtags.get_tiktoks_by_hashtag('fyp', count=1)
 
         # Download and send the first video
-        video = trending_videos[0]
+        video = videos[0]
         async with httpx.AsyncClient() as client:
-            response = await client.get(video['video']['downloadAddr'])
+            response = await client.get(video['videoUrl'])
             video_data = response.content
 
         with tempfile.NamedTemporaryFile(delete=False) as temp_file:
@@ -27,8 +27,8 @@ class fyp(commands.Cog):
             temp_file.seek(0)
 
             # Create an embed with video details
-            embed = discord.Embed(title=video['desc'], color=0x00ff00)
-            embed.set_author(name=video['author']['uniqueId'])
+            embed = discord.Embed(title=video['text'], color=0x00ff00)
+            embed.set_author(name=video['author']['username'])
             embed.set_footer(text=f"Uploaded on {video['createTime']}")
 
             # Send the video and embed to the chat
@@ -36,6 +36,7 @@ class fyp(commands.Cog):
 
             # Clean up the temporary file
             os.unlink(temp_file.name)
+
 
 
 
