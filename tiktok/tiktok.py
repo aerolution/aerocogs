161f9aea9b5a409865aaf40f817113503db14ff3
3 <74258466+aerolution@users.noreply.github.com>
2023-05-11 23:17:11 -0400

    Update tiktok.py

--- tiktok/tiktok.py
+++ tiktok/tiktok.py
@@ -2,40 +2,25 @@ import os
 import asyncio
 import discord
 from redbot.core import commands
-from tiktokpy import TikTokPy
+from tiktokapi import TikTokApi
 
-api = TikTokPy({
-  'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
-  'Accept-Language': 'en-US,en;q=0.5',
-  'Accept-Encoding': 'gzip, deflate, br',
-  'Upgrade-Insecure-Requests': '1'
-})
+api = TikTokApi.get_instance()
 
 class MyCog(commands.Cog):
-    """ A custom cog to get trending TikTok videos """
+    """Get trending TikTok videos"""
 
     def __init__(self, bot):
         self.bot = bot
 
     @commands.command()
     async def tiktok(self, ctx):
-        """ Get the trending TikTok videos """
-        print('Executing tiktok command...')
-        try:
-            # Fetch the trending videos from TikTok API
-            trending = api.trending(count=5)
-
-            # Filter the videos with #dogs and #cats hashtags
-            filtered = [video for video in trending if "#dogs" in video['desc'] and "#cats" in video['desc']]
-
-            # Create the response message
-            response = ""
-            for i, video in enumerate(filtered):
-                response += f"{i+1}. {video['desc']} - {video['video']['urls'][0]}\n"
-
-            print('Sending response...')
-            # Send the response to the Discord channel
-            await ctx.send(response)
-
-        except Exception as e:
-            print('Error:', e)
+        """Get the trending TikTok videos"""
+        trending = api.trending(count=5)
+        # Filter the videos with dogs and cats hashtags
+        filtered = [video for video in trending if "#dogs" in video['desc'] and "#cats" in video['desc']]
+        # Create the response message
+        response = ""
+        for i, video in enumerate(filtered):
+            response += f"{i+1}. {video['desc']} - {video['video']['downloadAddr']}\n"
+        # Send the response to the Discord channel
+        await ctx.send(response)
