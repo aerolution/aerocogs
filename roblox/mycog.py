ca437c2281bf3645d72e84652cfdebea67ca556e
3 <74258466+aerolution@users.noreply.github.com>
2023-05-11 22:00:07 -0400

    Update mycog.py

--- roblox/mycog.py
+++ roblox/mycog.py
@@ -7,7 +7,26 @@ class MyCog(commands.Cog):
         self.bot = bot
 
     @commands.command()
-    async def roblox(self, ctx):
-        """This does stuff!"""
-        # Your code will go here
-        await ctx.send("I can do stuff!")
+    async def roblox(ctx, username):
+
+ carros_json=requests.get(f"https://api.roblox.com/users/get-by-u...{username}")
+ carros=json.loads(carros_json.text)
+ test = (carros["Id"])
+ #image
+ carro_json2=requests.get(f"https://users.roblox.com/v1/users/{test}/status")
+ carro2=json.loads(carro_json2.text)
+ test2 = (carro2["status"])
+
+ #display
+ displayname2=requests.get(f"https://users.roblox.com/v1/users/{test}")
+ displayname3=json.loads(displayname2.text)
+ displayname4 = (displayname3["displayName"])
+ #created
+ date1=requests.get(f"https://users.roblox.com/v1/users/{test}")
+ date2=json.loads(date1.text)
+ date3 = (date2["created"])
+
+ #description
+ desc1=requests.get(f"https://users.roblox.com/v1/users/{test}")
+ desc2=json.loads(desc1.text)
+ desc3 = (desc2["description"])
