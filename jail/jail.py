f62bc7c40a150f7a946ce4b6395eab2fb59b1fee
3 <74258466+aerolution@users.noreply.github.com>
2023-05-20 17:15:24 -0400

    Update jail.py

--- jail/jail.py
+++ jail/jail.py
@@ -1,22 +1,40 @@
-from discord.ext import commands
-from discord.utils import get
 import discord
+from redbot.core import commands, Config
 
-class JailCog(commands.Cog):
+
+class Jail(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.config = Config.get_conf(self, identifier=1234567890)
+        default_guild = {"jail_channel": None}
+        self.config.register_guild(**default_guild)
 
+    @commands.guild_only()
+    @commands.has_permissions(manage_roles=True)
     @commands.command()
-    async def jail(self, ctx, member: discord.Member):
-        server_id = 1093594511076249701
-        role_id = 1098752253449486416
+    async def jail(self, ctx, member: discord.Member, reason: str):
+        """Jail a user and restrict them to a single specified channel."""
+        jail_channel_id = await self.config.guild(ctx.guild).jail_channel()
+        if not jail_channel_id:
+            await ctx.send("Jail channel not set. Please set it using `setjailchannel` command.")
+            return
 
-        if ctx.guild.id == server_id:
-            role = get(ctx.guild.roles, id=role_id)
-            if role is not None:
-                await member.add_roles(role)
-                await ctx.send(f"{member.mention} has been jailed.")
-            else:
-                await ctx.send(f"Role with ID {role_id} not found.")
-        else:
-            await ctx.send(f"This command can only be used in the server with ID {server_id}.")
+        jail_channel = ctx.guild.get_channel(jail_channel_id)
+        if not jail_channel:
+            await ctx.send("Jail channel not found. Please set a valid channel using `setjailchannel` command.")
+            return
+
+        overwrites = {
+            ctx.guild.default_role: discord.PermissionOverwrite(read_messages=False),
+            member: discord.PermissionOverwrite(read_messages=True)
+        }
+        await jail_channel.edit(overwrites=overwrites)
+        await ctx.send(f"{member.mention} has been jailed for {reason}.")
+
+    @commands.guild_only()
+    @commands.has_permissions(manage_channels=True)
+    @commands.command()
+    async def setjailchannel(self, ctx, channel: discord.TextChannel):
+        """Set the jail channel for this server."""
+        await self.config.guild(ctx.guild).jail_channel.set(channel.id)
+        await ctx.send(f"Jail channel has been set to {channel.mention}.")
