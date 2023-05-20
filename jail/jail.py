edb6fed0459544482d7679dacd8df393d18cdb11
3 <74258466+aerolution@users.noreply.github.com>
2023-05-20 18:21:04 -0400

    Update jail.py

--- jail/jail.py
+++ jail/jail.py
@@ -19,84 +19,73 @@ class Jail(commands.Cog):
     @commands.guild_only()
     @commands.has_permissions(manage_roles=True)
     @commands.command()
+    async def setjailchannel(self, ctx, channel: discord.TextChannel):
+        """Set the channel to use as the jail."""
+        await self.config.guild(ctx.guild).jail_channel.set(channel.id)
+        await ctx.send(f"The jail channel has been set to {channel.mention}.")
+
+    @commands.guild_only()
+    @commands.has_permissions(manage_roles=True)
+    @commands.command()
+    async def setjaillogchannel(self, ctx, channel: discord.TextChannel):
+        """Set the channel to use for jail logs."""
+        await self.config.guild(ctx.guild).jail_log_channel.set(channel.id)
+        await ctx.send(f"The jail log channel has been set to {channel.mention}.")
+
+    @commands.guild_only()
+    @commands.has_permissions(manage_roles=True)
+    @commands.command()
     async def jail(self, ctx, member: discord.Member, *, reason: str):
         """Jail a user and restrict them to a single specified channel."""
         jail_channel_id = await self.config.guild(ctx.guild).jail_channel()
-        if not jail_channel_id:
-            await ctx.send("Jail channel not set. Please set it using `setjailchannel` command.")
-            return
-
         jail_channel = ctx.guild.get_channel(jail_channel_id)
-        if not jail_channel:
-            await ctx.send("Jail channel not found. Please set a valid channel using `setjailchannel` command.")
-            return
 
-        default_role = ctx.guild.default_role
-        default_role_overwrite = jail_channel.overwrites_for(default_role)
-        default_role_overwrite.send_messages = False
+        # Get all channels in the guild
+        channels = ctx.guild.channels
 
-        member_overwrite = jail_channel.overwrites_for(member)
-        member_overwrite.send_messages = True
+        # Deny send messages and view channel permissions in all channels
+        for channel in channels:
+            await channel.set_permissions(member, send_messages=False, view_channel=False)
 
-        await jail_channel.set_permissions(default_role, overwrite=default_role_overwrite, reason=reason)
-        await jail_channel.set_permissions(member, overwrite=member_overwrite, reason=reason)
+        # Allow send messages permission only in the jail channel
+        await jail_channel.set_permissions(member, send_messages=True)
 
         # Create an embed message
         embed = discord.Embed(title="User Jailed", color=discord.Color.red())
         embed.add_field(name="User", value=member.mention, inline=False)
         embed.add_field(name="Reason", value=reason, inline=False)
-        embed.set_footer(text=f"Jailed by {ctx.author}", icon_url=ctx.message.author.avatar)
+        embed.set_footer(text=f"Jailed by {author}", icon_url=author.avatar)
 
         # Send the embed message to the context channel
         await ctx.send(embed=embed)
 
         # Notify the jail log channel
-        await self.notify_log_channel(ctx.guild, f"{member.mention} has been jailed for {reason} by {ctx.author.mention}.")
+        await self.notify_log_channel(ctx.guild, f"{member.mention} has been jailed for {reason}.")
 
     @commands.guild_only()
     @commands.has_permissions(manage_roles=True)
     @commands.command()
     async def unjail(self, ctx, member: discord.Member, *, reason: str):
         """Unjail a user and restore their permissions."""
-        jail_channel_id = await self.config.guild(ctx.guild).jail_channel()
-        if not jail_channel_id:
-            await ctx.send("Jail channel not set. Please set it using `setjailchannel` command.")
-            return
+        # Get all channels in the guild
+        channels = ctx.guild.channels
 
-        jail_channel = ctx.guild.get_channel(jail_channel_id)
-        if not jail_channel:
-            await ctx.send("Jail channel not found. Please set a valid channel using `setjailchannel` command.")
-            return
+        # Remove permission overwrites for the member in all channels 
+        for channel in channels:
+            await channel.set_permissions(member, overwrite=None)
 
-        await jail_channel.set_permissions(member, overwrite=None, reason=reason)
-        
-        # message author
-        author = ctx.message.author
-        
         # Create an embed message
         embed = discord.Embed(title="User Unjailed", color=discord.Color.green())
         embed.add_field(name="User", value=member.mention, inline=False)
         embed.add_field(name="Reason", value=reason, inline=False)
+
+        # Get the actual command author
+        author = ctx.message.author  
+
         embed.set_footer(text=f"Unjailed by {author}", icon_url=author.avatar)
 
         # Send the embed message to the context channel
         await ctx.send(embed=embed)
 
         # Notify the jail log channel
-        await self.notify_log_channel(ctx.guild, f"{member.mention} has been unjailed for {reason} by {ctx.author.mention}.")
-
-    @commands.guild_only()
-    @commands.has_permissions(manage_channels=True)
-    @commands.command()
-    async def setjailchannel(self, ctx, channel: discord.TextChannel):
-        """Set the jail channel for this server."""
-        await self.config.guild(ctx.guild).jail_channel.set(channel.id)
-        await ctx.send(f"Jail channel has been set to {channel.mention}.")
-
-    @commands.guild_only()
-    @commands.has_permissions(manage_channels=True)
-    @commands.command()
-    async def setjaillogchannel(self, ctx, channel: discord.TextChannel):
-        """Set the jail log channel for this server."""
-        await self.config.guild(ctx.guild).jail_log_channel.set(channel.id)
-        await ctx.send(f"Jail log channel has been set to {channel.mention}.")
+        await self.notify_log_channel(ctx.guild, f"{member.mention} has been unjailed for {reason} by {author.mention}.")
