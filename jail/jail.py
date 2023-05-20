ad179e348e110896e5d1c6666bf28bdc5fe4e963
3 <74258466+aerolution@users.noreply.github.com>
2023-05-20 17:54:00 -0400

    Update jail.py

--- jail/jail.py
+++ jail/jail.py
@@ -6,8 +6,15 @@ class Jail(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1234567890)
-        default_guild = {"jail_channel": None}
+        default_guild = {"jail_channel": None, "jail_log_channel": None}
         self.config.register_guild(**default_guild)
+
+    async def notify_log_channel(self, guild, message):
+        log_channel_id = await self.config.guild(guild).jail_log_channel()
+        if log_channel_id:
+            log_channel = guild.get_channel(log_channel_id)
+            if log_channel:
+                await log_channel.send(message)
 
     @commands.guild_only()
     @commands.has_permissions(manage_roles=True)
@@ -33,7 +40,47 @@ class Jail(commands.Cog):
 
         await jail_channel.set_permissions(default_role, overwrite=default_role_overwrite, reason=reason)
         await jail_channel.set_permissions(member, overwrite=member_overwrite, reason=reason)
-        await ctx.send(f"{member.mention} has been jailed for {reason}.")
+
+        # Create an embed message
+        embed = discord.Embed(title="User Jailed", color=discord.Color.red())
+        embed.add_field(name="User", value=member.mention, inline=False)
+        embed.add_field(name="Reason", value=reason, inline=False)
+        embed.set_footer(text=f"Jailed by {ctx.author}", icon_url=ctx.author.avatar_url)
+
+        # Send the embed message to the context channel
+        await ctx.send(embed=embed)
+
+        # Notify the jail log channel
+        await self.notify_log_channel(ctx.guild, f"{member.mention} has been jailed for {reason} by {ctx.author.mention}.")
+
+    @commands.guild_only()
+    @commands.has_permissions(manage_roles=True)
+    @commands.command()
+    async def unjail(self, ctx, member: discord.Member, *, reason: str):
+        """Unjail a user and restore their permissions."""
+        jail_channel_id = await self.config.guild(ctx.guild).jail_channel()
+        if not jail_channel_id:
+            await ctx.send("Jail channel not set. Please set it using `setjailchannel` command.")
+            return
+
+        jail_channel = ctx.guild.get_channel(jail_channel_id)
+        if not jail_channel:
+            await ctx.send("Jail channel not found. Please set a valid channel using `setjailchannel` command.")
+            return
+
+        await jail_channel.set_permissions(member, overwrite=None, reason=reason)
+
+        # Create an embed message
+        embed = discord.Embed(title="User Unjailed", color=discord.Color.green())
+        embed.add_field(name="User", value=member.mention, inline=False)
+        embed.add_field(name="Reason", value=reason, inline=False)
+        embed.set_footer(text=f"Unjailed by {ctx.author}", icon_url=ctx.author.avatar_url)
+
+        # Send the embed message to the context channel
+        await ctx.send(embed=embed)
+
+        # Notify the jail log channel
+        await self.notify_log_channel(ctx.guild, f"{member.mention} has been unjailed for {reason} by {ctx.author.mention}.")
 
     @commands.guild_only()
     @commands.has_permissions(manage_channels=True)
@@ -42,3 +89,11 @@ class Jail(commands.Cog):
         """Set the jail channel for this server."""
         await self.config.guild(ctx.guild).jail_channel.set(channel.id)
         await ctx.send(f"Jail channel has been set to {channel.mention}.")
+
+    @commands.guild_only()
+    @commands.has_permissions(manage_channels=True)
+    @commands.command()
+    async def setjaillogchannel(self, ctx, channel: discord.TextChannel):
+        """Set the jail log channel for this server."""
+        await self.config.guild(ctx.guild).jail_log_channel.set(channel.id)
+        await ctx.send(f"Jail log channel has been set to {channel.mention}.")
