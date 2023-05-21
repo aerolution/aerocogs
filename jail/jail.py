6f15199b6cb9f4fb89b6e06b7c38c7ce5af31980
3 <74258466+aerolution@users.noreply.github.com>
2023-05-21 14:31:53 -0400

    Update jail.py

--- jail/jail.py
+++ jail/jail.py
@@ -1,24 +1,25 @@
 import discord
 from redbot.core import commands, Config
+import asyncio
 
 
 class Jail(commands.Cog):
     """
     Put users in a jail! (Channel.)
     """
-        
+
     def __init__(self, bot):
         self.bot = bot
         self.config = Config.get_conf(self, identifier=1234567890)
         default_guild = {"jail_channel": None, "jail_log_channel": None}
         self.config.register_guild(**default_guild)
 
-    async def notify_log_channel(self, guild, message):
+    async def notify_log_channel(self, guild, embed):
         log_channel_id = await self.config.guild(guild).jail_log_channel()
         if log_channel_id:
             log_channel = guild.get_channel(log_channel_id)
             if log_channel:
-                await log_channel.send(message)
+                await log_channel.send(embed=embed)
 
     @commands.guild_only()
     @commands.has_permissions(manage_roles=True)
@@ -39,11 +40,11 @@ class Jail(commands.Cog):
     @commands.guild_only()
     @commands.has_permissions(manage_roles=True)
     @commands.command()
-    async def jail(self, ctx, member: discord.Member, *, reason: str):
+    async def jail(self, ctx, member: discord.Member, reason: str = None, time: int = None):
         """Jail a user and restrict them to a single specified channel."""
         jail_channel_id = await self.config.guild(ctx.guild).jail_channel()
         jail_channel = ctx.guild.get_channel(jail_channel_id)
-       
+
         # get author
         author = ctx.message.author
 
@@ -60,39 +61,47 @@ class Jail(commands.Cog):
         # Create an embed message
         embed = discord.Embed(title="User Jailed", color=discord.Color.red())
         embed.add_field(name="User", value=member.mention, inline=False)
-        embed.add_field(name="Reason", value=reason, inline=False)
-        embed.set_footer(text=f"Jailed by {author}", icon_url=author.avatar)
+        if reason:
+            embed.add_field(name="Reason", value=reason, inline=False)
+        if time:
+            embed.add_field(name="Time", value=f"{time} seconds", inline=False)
+        embed.set_footer(text=f"Jailed by {author}", icon_url=author.avatar_url)
 
         # Send the embed message to the context channel
         await ctx.send(embed=embed)
 
         # Notify the jail log channel
-        await self.notify_log_channel(ctx.guild, f"{member.mention} has been jailed for {reason}.")
+        await self.notify_log_channel(ctx.guild, embed)
+
+        if time:
+            await asyncio.sleep(time)
+            await self.unjail(ctx, member, reason="Time served")
 
     @commands.guild_only()
     @commands.has_permissions(manage_roles=True)
     @commands.command()
-    async def unjail(self, ctx, member: discord.Member, *, reason: str):
+    async def unjail(self, ctx, member: discord.Member, *, reason: str = None):
         """Unjail a user and restore their permissions."""
         # Get all channels in the guild
         channels = ctx.guild.channels
 
-        # Remove permission overwrites for the member in all channels 
+        # Remove permission overwrites for the member in all channels
         for channel in channels:
             await channel.set_permissions(member, overwrite=None)
 
         # Create an embed message
         embed = discord.Embed(title="User Unjailed", color=discord.Color.green())
         embed.add_field(name="User", value=member.mention, inline=False)
-        embed.add_field(name="Reason", value=reason, inline=False)
+        if reason:
+            embed.add_field(name="Reason", value=reason, inline=False)
 
         # Get the actual command author
-        author = ctx.message.author  
+        author = ctx.message.author
 
-        embed.set_footer(text=f"Unjailed by {author}", icon_url=author.avatar)
+        embed.set_footer(text=f"Unjailed by {author}", icon_url=author.avatar_url)
 
         # Send the embed message to the context channel
         await ctx.send(embed=embed)
 
         # Notify the jail log channel
-        await self.notify_log_channel(ctx.guild, f"{member.mention} has been unjailed for {reason} by {author.mention}.")
+        await self.notify_log_channel(ctx.guild, embed)
