8d23b56becbb056613497456754a2e300a78f283
3 <74258466+aerolution@users.noreply.github.com>
2023-06-06 16:13:32 -0400

    Update vanity.py

--- vanity/vanity.py
+++ vanity/vanity.py
@@ -1,12 +1,13 @@
 import asyncio
 from redbot.core import commands
 import os
+import discord
 
 class VanityChecker(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
         self.vanity_list = self.load_vanity_list()
-        self.channel_id = 1115063658511355944
+        self.channel_id = 1115063675632484452
         self.check_interval = 3600  # Check every hour (in seconds)
         self.bot.loop.create_task(self.check_vanities())
 
@@ -20,17 +21,23 @@ class VanityChecker(commands.Cog):
 
 
     async def check_vanities(self):
-        await self.bot.wait_until_ready()
-        while not self.bot.is_closed():
-            for vanity in self.vanity_list:
-                if await self.is_vanity_free(vanity):
-                    await self.log_vanity(vanity)
-            await asyncio.sleep(self.check_interval)
+    await self.bot.wait_until_ready()
+    while not self.bot.is_closed():
+        for vanity in self.vanity_list:
+            if await self.is_vanity_free(vanity):
+                await self.log_vanity(vanity)
+        await asyncio.sleep(self.check_interval)
 
-    async def is_vanity_free(self, vanity):
-        # Implement the logic to check if the vanity URL is free
-        # You may need to use Discord API or a third-party service to check the availability
-        pass
+            
+async def is_vanity_free(self, vanity):
+    try:
+        invite = await self.bot.fetch_invite(f"https://discord.gg/{vanity}")
+    except discord.NotFound:
+        # The invite was not found, which means the vanity URL is available
+        return True
+    else:
+        # The invite was found, which means the vanity URL is not available
+        return False
 
     async def log_vanity(self, vanity):
         channel = self.bot.get_channel(self.channel_id)
