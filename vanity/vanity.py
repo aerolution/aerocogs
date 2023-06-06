9f0dbc929673f00093d6b1e7db7789f206f5fff4
3 <74258466+aerolution@users.noreply.github.com>
2023-06-06 15:50:11 -0400

    Create vanity.py

---
+++ vanity/vanity.py
@@ -0,0 +1,35 @@
+import asyncio
+from redbot.core import commands
+
+class VanityChecker(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        self.vanity_list = self.load_vanity_list()
+        self.channel_id = 1115063658511355944
+        self.check_interval = 3600  # Check every hour (in seconds)
+        self.bot.loop.create_task(self.check_vanities())
+
+    def load_vanity_list(self):
+        # Load your extremely long list of words from a file or an external source
+        # For example, you can load a text file with one word per line
+        with open("./word_list.txt", "r") as f:
+            words = [line.strip() for line in f.readlines()]
+        return words
+
+    async def check_vanities(self):
+        await self.bot.wait_until_ready()
+        while not self.bot.is_closed():
+            for vanity in self.vanity_list:
+                if await self.is_vanity_free(vanity):
+                    await self.log_vanity(vanity)
+            await asyncio.sleep(self.check_interval)
+
+    async def is_vanity_free(self, vanity):
+        # Implement the logic to check if the vanity URL is free
+        # You may need to use Discord API or a third-party service to check the availability
+        pass
+
+    async def log_vanity(self, vanity):
+        channel = self.bot.get_channel(self.channel_id)
+        if channel:
+            await channel.send(f"This vanity is free: https://discord.gg/{vanity}")
